ed49ed4a9c9c6b8b47a9575e0223b90101a62e82
saaandy3D <alexandra.francisco@divx.co.jp>
2022-11-07 18:01:18 +0900

    セクション2-22

--- main.js
+++ main.js
@@ -87,4 +87,23 @@ let message2 = `こんにちは ${lastName} ${firstName} さん
 JavaScriptの学習を始めましょう！`;
 console.log(message2);
 
+let num0 = 20;
+num0 = num0 + 10;
+console.log(num0);
+
+let num1 = 20;
+num1 += 10;
+console.log(num1);
+
+let num2 = 20;
+num2 -= 10;
+console.log(num2);
+
+let num3 = 20;
+num3 *= 10;
+console.log(num3);
+
+let num4 = 20;
+num4 /= 10;
+console.log(num4);
 
