caf704906290f3f5fae4527816ebc69ee994530d
saaandy3D <alexandra.francisco@divx.co.jp>
2022-11-07 17:35:26 +0900

    セクション2-21+（本当はプッシュやり直したい）

--- main.js
+++ main.js
@@ -82,3 +82,9 @@ let lastName = 'なかむら';
 let firstName = 'ゆうた';
 let message1 = 'こんにちは' + lastName + '' + firstName + 'さん';
 console.log(message1);
+
+let message2 = `こんにちは ${lastName} ${firstName} さん
+JavaScriptの学習を始めましょう！`;
+console.log(message2);
+
+
