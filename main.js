79acdb89b8ce34831f8b5913bcedad6ebdfbbb51
saaandy3D <alexandra.francisco@divx.co.jp>
2022-11-08 12:23:38 +0900

    セクション3-47

--- main.js
+++ main.js
@@ -390,7 +390,6 @@ function inputUserName(callback) {
 	callback(name);
 }
 inputUserName(greeting);
-*/
 
 let colors = ['Red', 'Green', 'Blue'];
 console.log(colors);
@@ -410,3 +409,41 @@ console.log(arr);
 //からの配列
 let emptyArr = [];
 console.log(emptyArr);
+*/
+
+let colors = ['Red', 'Green', 'Blue'];
+console.log(colors);
+
+//末尾に追加
+colors.push('Black');
+console.log(colors);
+//先頭に追加
+colors.unshift('White');
+console.log(colors);
+//値の更新
+colors[1] = 'Aka';
+console.log(colors);
+
+//指定した位置に1つ以上の値の挿入
+let insertArr = ['a', 'b', 'c'];
+insertArr.splice(1, 0, 'D', 'E', 'F');
+console.log(insertArr);
+//指定した位置から削除
+insertArr.splice(1, 2);
+console.log(insertArr);
+
+//結合
+let arr1 = ['a', 'b', 'c'];
+let arr2 = ['d', 'e', 'f'];
+let mergedArr = arr1.concat(arr2);
+console.log(mergedArr);
+console.log(arr1);
+console.log(arr2);
+
+//先頭の削除
+let arr3 = ['a', 'b', 'c', 'd'];
+arr3.shift();
+console.log(arr3);
+//末尾を削除
+arr3.pop();
+console.log(arr3);
