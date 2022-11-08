c0e3bad1b80571a1cc06db86de39df177d20acc5
saaandy3D <alexandra.francisco@divx.co.jp>
2022-11-08 16:18:56 +0900

    セクション4-61

--- main.js
+++ main.js
@@ -2,6 +2,21 @@
 
 let btn = document.getElementById('triggerButton');
 btn.addEventListener('click', function(e){
+	//IDによるノードの取得
+	let inputColorName = document.getElementById('inputColorName');
+	//取得したinput要素の入力値を取得
+	let colorNameText = inputColorName.value;
+	//取得したinput要素のname属性値を取得
+	let inputName = inputColorName.getAttribute('name');
+	//idがshowTextのp要素を取得
+	let showText = document.getElementById('showText');
+	//出力
+	showText.textContent = `name属性: ${inputName}, 入力されたテキスト: ${colorNameText}`;
+}, false);
+
+/*
+let btn = document.getElementById('triggerButton');
+btn.addEventListener('click', function(e){
 	let headerTitle = document.getElementById('headerTitle');
 	//テキストの置換
 	headerTitle.textContent = 'タイトルが変更されました';
