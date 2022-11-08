15799021da78a9db0603b887d33a74d3bd2cd731
saaandy3D <alexandra.francisco@divx.co.jp>
2022-11-08 14:30:10 +0900

    セクション3-51 ?forの入れ子

--- main.js
+++ main.js
@@ -505,7 +505,6 @@ let user = {
 	}
 }
 console.log(user.calcAge(2022));
-*/
 
 // 例題
 for(let i = 1; i <= 3; i++) {
@@ -515,4 +514,33 @@ for(let i = 1; i <= 3; i++) {
 let arr = ['a', 'b', 'c']
 for(let j = 0; j < arr.length; j++) {
 	console.log(arr[j]);
+}
+*/
+
+/*for(let i = 0; i < 3; i++) {
+	for(let j = 0; j < 3; j++) {
+		console.log(`i: ${i}, j: ${j}`);
+	}
+}*/
+
+//例題
+const scores = [
+	[100, 99, 98],
+	[90, 89, 88],
+	[80, 79, 78]
+];
+//console.log(scores[0][0]);
+//console.log(scores[0][1]);
+//console.log(scores[0][2]);
+//console.log(scores[1][0]);
+//console.log(scores[1][2]);
+//console.log(scores[1][2]);
+//console.log(scores[2][0]);
+//console.log(scores[2][1]);
+//console.log(scores[2][2]);
+
+for (let i = 0; i < scores.length; i++) {
+	for(let j  = 0; j < scores[i].length; j++) {
+		console.log(scores[i][j]);
+	}
 }
