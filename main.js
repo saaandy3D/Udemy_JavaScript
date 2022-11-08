40876aa56014a5d1f5a0a0bc24f31233e2f96a5c
saaandy3D <alexandra.francisco@divx.co.jp>
2022-11-08 12:48:39 +0900

    セクション3-48

--- main.js
+++ main.js
@@ -409,7 +409,6 @@ console.log(arr);
 //からの配列
 let emptyArr = [];
 console.log(emptyArr);
-*/
 
 let colors = ['Red', 'Green', 'Blue'];
 console.log(colors);
@@ -447,3 +446,51 @@ console.log(arr3);
 //末尾を削除
 arr3.pop();
 console.log(arr3);
+
+//例題
+let user = new Object();
+user.name = 'Taro Yamada';
+user.gender = 'man'
+user.birthYear = 1990;
+console.log(user)
+*/
+
+//オブジェクト初期化子
+let user = { name: 'Taro Yamada', gender: 'man' , birthYear: 1990};
+console.log(user);
+//ドット記法
+console.log(user.name);
+console.log(user.gender);
+console.log(user.birthYear);
+//ブラケット記法
+console.log(user['name']);
+console.log(user['gender']);
+console.log(user['birthYear']);
+
+//個数
+console.log(Object.keys(user).length);
+
+//プロパティ（key）を取得
+let keys = Object.keys(user);
+console.log(keys);
+
+//追加
+//user.prefecture = 'Kanagawa';
+user['prefecture'] = 'Kanagawa';
+console.log(user);
+
+//値の書き換え
+//user.prefecture = 'Chiba';
+user['prefecture'] = 'Chiba';
+console.log(user);
+
+//結合
+let obj1 = { a: 1, b: 2, c: 3 };
+let obj2 = { d: 4, e: 5, f: 6 };
+let mergedObj = Object.assign(obj1, obj2);
+console.log(mergedObj);
+//obj1にもobj2の内容が追加される
+
+//空のオブジェクト
+let person = {};
+console.log(person);
