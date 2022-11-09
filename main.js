2d5daaaac8d50d6b4a475820480c738973689bb4
saaandy3D <alexandra.francisco@divx.co.jp>
2022-11-09 14:13:25 +0900

    セクション7 /見ながら実装/

--- main.js
+++ main.js
@@ -1,5 +1,48 @@
 'use strict';
 
+const images = [
+  {src: 'https://learnwithnakamura.s3.ap-northeast-1.amazonaws.com/javascript/C0zDWAPFT9A.jpg', description: '猫の画像1'},
+  {src: 'https://learnwithnakamura.s3.ap-northeast-1.amazonaws.com/javascript/fEK4jvgnApg.jpg', description: '猫の画像2'},
+  {src: 'https://learnwithnakamura.s3.ap-northeast-1.amazonaws.com/javascript/egfS7HzgKcc.jpg', description: '猫の画像3'},
+  {src: 'https://learnwithnakamura.s3.ap-northeast-1.amazonaws.com/javascript/e-S23SJzFqs.jpg', description: '猫の画像4'},
+  {src: 'https://learnwithnakamura.s3.ap-northeast-1.amazonaws.com/javascript/Hnwm8ktAd6E.jpg', description: '猫の画像5'}
+];
+
+//メイン画像の初期値を設定
+let image = document.createElement('img');
+image.setAttribute('src', images[0].src);
+image.setAttribute('alt', images[0].description);
+
+//説明の初期値を設定
+let description = document.createElement('p');
+description.textContent = image.alt;
+
+//作生成した要素を表示する
+let mainImage = document.getElementById('main_image');
+mainImage.insertBefore(image, null);
+mainImage.insertBefore(description, null);
+
+//サムネイル画像の表示
+let thumbnails = document.getElementById('thumbnails');
+for(let i = 0; i < images.length; i++) {
+	let thumbnailImage = document.createElement('img');
+	thumbnailImage.setAttribute('src', images[i].src);
+	thumbnailImage.setAttribute('alt', images[i].description);
+	thumbnails.insertBefore(thumbnailImage, null);
+}
+
+//クリックしたサムネイル画像をメイン画像にする
+thumbnails.addEventListener('click', function(e) {
+	if(e.target.src) {
+	//console.log(e.target);
+	image.src = e.target.src;
+	description.textContent = e.target.alt;
+	}
+}, false);
+
+
+
+/*
 function keyUp(e) {
 	//console.log('keyUp');
 	//console.log(inputText.value);
@@ -27,10 +70,6 @@ function keyUp(e) {
 const inputText = document.getElementById('inputText');
 inputText.addEventListener('keyup', keyUp, false);
 
-
-
-
-/*
 let x = 0;
 
 number.textContent = x;
